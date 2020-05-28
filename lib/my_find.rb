8e7b18bb41677ae2b79be2337ff27753617aac60
Tanya Hodges <tanyac52003@gmail.com>
2020-05-28 16:29:19 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_find(collection)
-
+  index = 0 
+  while index < collection.length 
+  if yield(collection[index])
+    return (collection[index])
+  end 
+  index += 1 
+  end 
 end
